52cc1007741be030aa54f7c65b9e47fba4a839f8
rafu1987 <rafu1987@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-28 18:27:38 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rzlightbox/trunk@30618 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "rzlightbox"
+#
+# Auto generated 14-02-2010 13:12
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'jQuery Lightbox',
+	'description' => 'The extension includes the jQuery Lightbox for images.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.0.4',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Raphael Zschorsch',
+	'author_email' => 'rafu1987@gmail.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"3166";s:10:"README.txt";s:4:"d41d";s:12:"ext_icon.gif";s:4:"5b11";s:17:"ext_localconf.php";s:4:"0f76";s:14:"ext_tables.php";s:4:"b217";s:13:"locallang.xml";s:4:"17d9";s:16:"locallang_db.xml";s:4:"c3d3";s:14:"doc/manual.pdf";s:4:"1670";s:14:"doc/manual.sxw";s:4:"a5b6";s:19:"doc/wizard_form.dat";s:4:"cca3";s:20:"doc/wizard_form.html";s:4:"3121";s:31:"pi1/class.tx_rzlightbox_pi1.php";s:4:"cf20";s:17:"res/css/Thumbs.db";s:4:"2bc1";s:22:"res/css/closelabel.gif";s:4:"1daa";s:19:"res/css/loading.gif";s:4:"faa7";s:21:"res/css/nextlabel.gif";s:4:"485d";s:21:"res/css/prevlabel.gif";s:4:"d935";s:20:"res/css/slimbox2.css";s:4:"c4ba";s:20:"res/js/jquery-min.js";s:4:"9eb3";s:18:"res/js/slimbox2.js";s:4:"55e6";s:20:"static/constants.txt";s:4:"1550";s:16:"static/setup.txt";s:4:"28f6";}',
+	'suggests' => array(
+	),
+);
+
+?>
